2a22b7875b9aac773d58a84125a314fde93ee3dc
mirko <autieromir@gmail.com>
2023-01-14 17:32:26 +0100

    es-10

--- es-10/callback.mjs
+++ es-10/callback.mjs
@@ -1,8 +1,14 @@
 import * as fs from "node:fs";
 
-fs.writeFile('./greeting.txt', 'Hello Mirko!', 'utf8', (error)=> {
+fs.writeFile('./greeting.txt', 'Hello Mirko!', 'utf8', (error, data)=> {
     if(error){
         console.log(error);
     }
-    console.log('Done.');
-})
+
+
+    fs.writeFile('./goodbye.txt', 'Goodbye Mirko', 'utf8', (error, data)=>{
+        if(error){
+            console.log(error);
+        }
+     });
+});
